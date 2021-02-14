d173345074c5f6eb6f8e4bbf0cfa4c62b0ed65da
Jacob Quinn <quinn.jacobd@gmail.com>
2021-02-13 17:43:27 -0700

    Add DataAPI.unwrap with default definition
    As discussed in
    https://github.com/JuliaData/CategoricalArrays.jl/issues/142.

--- src/DataAPI.jl
+++ src/DataAPI.jl
@@ -169,4 +169,18 @@ struct Cols{T<:Tuple}
     Cols(args...) = new{typeof(args)}(args)
 end
 
+"""
+    unwrap(x)
+
+For a given scalar argument `x`, potentially "unwrap" it to return the base wrapped value.
+Useful as a generic API for wrapper types when the original value is needed.
+
+The default definition just returns `x` itself, i.e. no unwrapping is performned.
+
+This generic function is owned by DataAPI.jl itself, which is the sole provider of the
+default definition.
+"""
+function unwrap end
+unwrap(x) = x
+
 end # module
