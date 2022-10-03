3d1671b0946a48b99d8ede713842aff2fcbc7ac3
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-10-03 09:04:13 +0200

    minor docstring fixes

--- src/DataAPI.jl
+++ src/DataAPI.jl
@@ -305,9 +305,11 @@ Throw an error if `col` is not a column of `x`.
     metadatasupport(T::Type)
 
 Return a `NamedTuple{(:read, :write), Tuple{Bool, Bool}}` indicating whether
-values of type `T` support metadata. The `read` field indicates whether
-reading metadata with the [`metadata`](@ref) and [`metadatakeys`]](@ref)
-functions is supported.
+values of type `T` support metadata.
+
+The `read` field indicates whether reading metadata with the [`metadata`](@ref)
+and [`metadatakeys`]](@ref) functions is supported.
+
 The `write` field indicates whether modifying metadata with the [`metadata!`](@ref),
 [`deletemetadata!`](@ref), and [`emptymetadata!`](@ref) functions is supported.
 """
@@ -317,9 +319,11 @@ metadatasupport(::Type) = (read=false, write=false)
     colmetadatasupport(T::Type)
 
 Return a `NamedTuple{(:read, :write), Tuple{Bool, Bool}}` indicating whether
-values of type `T` support column metadata. The `read` field indicates whether
-reading metadata with the [`colmetadata`](@ref) and [`colmetadatakeys`](@ref)
-functions is supported.
+values of type `T` support column metadata.
+
+The `read` field indicates whether reading metadata with the [`colmetadata`](@ref)
+and [`colmetadatakeys`](@ref) functions is supported.
+
 The `write` field indicates whether modifying metadata with the [`colmetadata!`](@ref),
 [`deletecolmetadata!`](@ref), and [`emptycolmetadata!`](@ref) functions is supported.
 """
