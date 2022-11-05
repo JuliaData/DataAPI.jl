e76adf749f700b800451c5b156cfce3495d571ea
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-11-05 14:24:47 +0100

    move description of not passing `col` to top

--- src/DataAPI.jl
+++ src/DataAPI.jl
@@ -429,7 +429,11 @@ function colmetadata end
 """
     colmetadata(x, [col]; style::Bool=false)
 
-Return a dictionary mapping all column metadata keys to metadata values
+If `col` is not passed return a dictionary mapping columns represented as
+`Symbol` that have associated metadata to dictionaries mapping all
+metadata keys to metadata values associated with table `x` for a given column.
+
+If `col` is passed a dictionary mapping all column metadata keys to metadata values
 associated with column `col` of table `x`. Throw an error if `x` does not
 support reading metadata for column `col` or column `col` is not present in `x`.
 
@@ -438,10 +442,6 @@ style is an additional information about the kind of metadata that is stored for
 the `key`.
 
 $STYLE_INFO
-
-If `col` is not passed return a dictionary mapping columns represented as
-`Symbol` that have associated metadata to dictionaries mapping all
-metadata keys to metadata values associated with table `x` for a given column.
 
 The returned dictionary may be freshly allocated on each call to `colmetadata`
 and is considered to be owned by `x` so it must not be modified.
