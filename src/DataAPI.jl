6fc6ae439552f86f11182bd3a3be4058bb734359
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-12-16 13:25:46 +0100

    Update src/DataAPI.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/DataAPI.jl
+++ src/DataAPI.jl
@@ -207,7 +207,7 @@ If the only positional argument is a `Function` `f` then select the columns whos
 names passed to the `f` predicate as strings return `true`.
 
 When multiple `cols` selectors are passed then the sets of columns selected by them
-are passed to `operator` callable as positional arguments.
+are passed to `operator` as positional arguments.
 `operator` should be a set operation function, like `union`, `intersect`, `setdiff`, and `symdiff`
 defined in Base Julia. By default `operator=union` in which case all columns matching
 at least one selector are returned.
